2cea029060ab3db17af6bf07d44653cd4ab6255c
eduardossoares <eduardo21.net@gmail.com>
2025-06-12 12:28:44 -0300

    cria `sort` em `find-lower.js`

--- find-lower.js
+++ find-lower.js
@@ -11,3 +11,18 @@ const findLow = (arr) => {
 
   return lowIndex;
 };
+
+const sort = (arr) => {
+  const ordeneredArr = [];
+  const arrayLength = arr.length;
+
+  for (let i = 0; i < arrayLength; i++) {
+    let low = findLow(arr);
+    ordeneredArr.push(arr.splice(low, 1)[0]);
+  }
+
+  return ordeneredArr;
+};
+
+const ordeneredArr = sort([1, 5, 3, 0, 55, 11]);
+console.log(ordeneredArr);
