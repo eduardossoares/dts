41bb6e54a8efb2545b3cf2f42c3e26403131dcdd
eduardossoares <eduardo21.net@gmail.com>
2025-06-15 22:17:32 -0300

    founded

--- recursive-divide-and-conquer.js
+++ recursive-divide-and-conquer.js
@@ -1,6 +1,6 @@
 const findLastSquare = (width, height) => {
   console.log(width, height);
-  if (width % height === 0) return console.log("Find!");
+  if (width % height === 0) return console.log("Founded!");
   findLastSquare(height, width % height);
 };
 findLastSquare(1680, 640);
